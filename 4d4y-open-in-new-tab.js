12563d142620ae6c433b1fe95923df63675be7da
GeBron <gebron@qq.com>
2026-09-17 16:07:47 +0800

    perf(4d4y): MutationObserver 回调加防抖，统一脚本元数据
    - Observer 回调经 200ms 防抖，避免动态加载时频繁全量扫描
    - 补充 @noframes / @license / @updateURL / @downloadURL
    - 版本 1.1 -> 1.2

--- 4d4y-open-in-new-tab.js
+++ 4d4y-open-in-new-tab.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         4D4Y 论坛帖子新标签页打开
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  强制 4D4Y 论坛帖子列表中的标题链接在点击时于新标签页打开
 // @author       GeBron
 // @match        *://www.4d4y.com/forum/forumdisplay.php*
 // @grant        none
+// @noframes
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/GeBron/greasy-fork-script/master/4d4y-open-in-new-tab.js
+// @downloadURL  https://raw.githubusercontent.com/GeBron/greasy-fork-script/master/4d4y-open-in-new-tab.js
 // ==/UserScript==
 
 (function () {
@@ -21,9 +25,17 @@
         });
     }
 
+    function debounce(fn, wait) {
+        let timer = null;
+        return function (...args) {
+            clearTimeout(timer);
+            timer = setTimeout(() => fn.apply(this, args), wait);
+        };
+    }
+
     applyTargetBlank();
 
-    const observer = new MutationObserver(applyTargetBlank);
+    const observer = new MutationObserver(debounce(applyTargetBlank, 200));
     if (document.body) {
         observer.observe(document.body, { childList: true, subtree: true });
     } else {
